021884ee5f2ea5906e30e032c450d24f7c52384f
Mohan <mohtim@dsiadmins-MBP-3.lan>
2025-03-19 14:48:04 -0400

    Hyperparameters tuning is done

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -42,15 +42,60 @@ class ModelTrainer:
                 "Linear Regression": LinearRegression(),
                 "Lasso": Lasso(),
                 "Ridge": Ridge(),
-                "K-Neighbors Regressor": KNeighborsRegressor(),
                 "Decision Tree": DecisionTreeRegressor(),
                 "Random Forest Regressor": RandomForestRegressor(),
+                "Gradient Boosting": GradientBoostingRegressor(),
                 "CatBoosting Regressor": CatBoostRegressor(verbose=False),
                 "AdaBoost Regressor": AdaBoostRegressor()
             }
 
+
+            params= {
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest Regressor":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "Lasso": {
+                    'alpha': [0.0001, 0.001, 0.01, 0.1, 1, 10, 100],
+                    'max_iter': [1000, 2000, 3000, 5000]
+                },
+    
+                "Ridge": {
+                    'alpha': [0.0001, 0.001, 0.01, 0.1, 1, 10, 100],
+                    'solver': ['auto', 'svd', 'cholesky', 'lsqr', 'sparse_cg', 'sag', 'saga', 'lbfgs']
+                },
+               
+                "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
+            }
+
             model_report:dict= evaluate_models(X_train = X_train, y_train = y_train, X_test = X_test, y_test = y_test,
-                                              models = models)
+                                              models = models, param = params)
             
             # To get the best model score from dict
             best_model_score = max(sorted(model_report.values()))
