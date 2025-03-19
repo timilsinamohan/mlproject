7fa2aee97bde0c58b6414a0180685454e08fc4ac
Mohan <mohtim@dsiadmins-MBP-3.lan>
2025-03-19 12:08:48 -0400

    Data Transformation is completed

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -10,9 +10,9 @@ from src.components.data_transformation import DataTransformationConfig
 
 @dataclass
 class DataIngestionConfig:
-    train_data_path: str=os.path.join('artifacts',"train.scv")
-    test_data_path: str=os.path.join('artifacts',"test.scv")
-    raw_data_path: str=os.path.join('artifacts',"data.scv")
+    train_data_path: str=os.path.join('artifacts',"train.csv")
+    test_data_path: str=os.path.join('artifacts',"test.csv")
+    raw_data_path: str=os.path.join('artifacts',"data.csv")
    
 class DataIngestion:
     def __init__(self):
